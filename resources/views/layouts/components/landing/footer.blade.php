328ef5e7763edabfcea95078a210c79574cd2847
Lance02345 <lancelevis19@gmail.com>
2025-09-08 08:57:23 +0300

    designedBy

--- resources/views/layouts/components/landing/footer.blade.php
+++ resources/views/layouts/components/landing/footer.blade.php
@@ -1,9 +1,9 @@
 
             <div class="text-center landing-main-footer py-3">
                 <span class="text-muted fs-15"> Copyright © <span id="year"></span> <a
-                    href="javascript:void(0);" class="text-primary fw-semibold"><u>ynex</u></a>.
+                    href="javascript:void(0);" class="text-primary fw-semibold"><u>lancelevis19@gmail.com</u></a>.
                     Designed with <span class="fa fa-heart text-danger"></span> by <a href="javascript:void(0);" class="text-primary fw-semibold"><u>
-                    Spruko</u>
+                    Bethwel Kiruok</u>
                     </a> All
                     rights
                     reserved
